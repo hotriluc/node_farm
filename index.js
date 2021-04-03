615237222fa5a0d29e9a6f9ab2c0bc4490fb8fe5
hotriluc <hotriluc97@gmail.com>
2021-04-03 20:49:11 +0300

    Replace placeholders with data

--- index.js
+++ index.js
@@ -18,11 +18,7 @@ fs.readFile('./txt/start.txt', 'utf-8',(e, data) => {
         fs.readFile(`./txt/append.txt`, 'utf-8',(e, data3) => {
             console.log(data3);
 
-            fs.writeFile('./txt/final.txt', `${data2}\n${data3}`, 'utf-8', err =>{
-                console.log('your file has been written');
-            })
-        });
-    }); 
+            fs.writeFile('./txt/final.txt', `${data2}\n${This overview page"
 });
 console.log('Will read file!');
 */
@@ -30,9 +26,32 @@ console.log('Will read file!');
 
 
 /*========SERVER========*/
-const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
-const productData = JSON.parse(data);
 
+const replaceTemplate = (temp, product) => {
+    let output = temp.replace(/{%IMAGE%}/g, product.image);
+    output = output.replace(/{%PRODUCTNAME%}/g, product.productName);
+    output = output.replace(/{%FROM%}/g, product.from);
+    output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
+    output = output.replace(/{%QUANTITY%}/g, product.quantity);
+    output = output.replace(/{%PRICE%}/g, product.price);
+    output = output.replace(/{%ID%}/g, product.id);
+    output = output.replace(/{%DESCRIPTION%}/g, product.description);
+
+    if (!product.organic)   output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic');
+
+    return output;
+}
+
+//Getting views
+const tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`, 'utf-8');
+const tempCard = fs.readFileSync(`${__dirname}/templates/template-card.html`, 'utf-8');
+const tempProduct = fs.readFileSync(`${__dirname}/templates/template-product.html`, 'utf-8');
+
+//Getting data
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
+const dataObj = JSON.parse(data);
+
+//Creating the server
 const server = http.createServer( (req, res)=>{
     // gettting URL
     const pathName = req.url;
@@ -40,7 +59,14 @@ const server = http.createServer( (req, res)=>{
     // Routing
     // Overview page
     if (pathName ==='/' || pathName === '/overview') {
-        res.end("This overview page");
+        res.writeHead(200, {'Content-type': 'text/html'})
+
+        // getting html code for each card in dataObj
+        // using join to get single string and replace placeholder on template overview
+        const cardsHtml = dataObj.map(el => replaceTemplate(tempCard, el)).join('');
+        const output = tempOverview.replace('{%PRODUCT_CARDS%}', cardsHtml);
+
+        res.end(output)
     // Product page
     } else if (pathName === '/product'){
         res.end("This is the product page");
